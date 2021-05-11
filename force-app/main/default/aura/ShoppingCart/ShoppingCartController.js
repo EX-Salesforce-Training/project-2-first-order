42a915d810162376c1b6f13880e57056bdd0d77f
abruce3411 <abruce3411@gmail.com>
2021-05-11 16:12:42 -0500

    updates 412

--- force-app/main/default/aura/ShoppingCart/ShoppingCartController.js
+++ force-app/main/default/aura/ShoppingCart/ShoppingCartController.js
@@ -2,8 +2,11 @@
     //we need to add our logic to take the correct product object from the list and add it to
     //the attribute cartList without overwriting existing items
     handleAddToCart : function(component, event, helper) {
+        let cartList = component.get("v.cartList");
       let itemAddedToCart = event.getParam("Store_Product__c");
-      helper.helperItemRetrieval();
+      cartList.push(itemAddedToCart);
+      component.set("v.cartList", cartList);
+      
         
 
 
