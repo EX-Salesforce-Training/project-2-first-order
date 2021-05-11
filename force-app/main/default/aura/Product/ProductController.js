c3aebdb6626159f0db932f95d7527f272b3af055
Phong Nguyen <43973882+Phasor2@users.noreply.github.com>
2021-05-11 07:05:55 -0700

    adding the jared's component

--- force-app/main/default/aura/Product/ProductController.js
+++ force-app/main/default/aura/Product/ProductController.js
@@ -2,7 +2,7 @@
 	productSelected : function(component, event, helper) {
         component.set("v.productName", "Clicked!");
         
-        let selectedEvent = component.getEvent("productSelected");
+        let selectedEvent = $A.get("e.c:ProductSelected");
         selectedEvent.setParam({"Product" : component.get("v.product")});
         selectedEvent.fire();
 	}
