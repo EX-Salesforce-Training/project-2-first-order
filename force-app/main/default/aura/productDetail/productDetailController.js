aa454db17fad819f5172dcbf145d48a3f05ab69b
DanTFree <47703077+DanTFree@users.noreply.github.com>
2021-05-11 09:37:56 -0700

    Added the ability to leave a review. Added event firing with the add to cart Button. Lightning event Name addToCart

--- force-app/main/default/aura/productDetail/productDetailController.js
+++ force-app/main/default/aura/productDetail/productDetailController.js
@@ -1,23 +1,20 @@
 ({
 	handleApplicationEvent : function(component, event, helper) {
 		let theProduct = event.getParam("product");
-        console.log(theProduct.Id);
-        console.log(JSON.stringify(theProduct));
-        let myStr = JSON.parse(JSON.stringify(theProduct));
-        console.log(myStr.Id);
         
-    	
         component.set("v.theItem", theProduct);
         component.set("v.toRender", true);
-        console.log(component.get("v.theItem").Id);
-        console.log(component.get("v.toRender"));
-        helper.getReviews(component, component.get("v.theItem"));
-        console.log(component.get("v.theItem"));
+        helper.getReviews(component, component.get("v.theItem").Id);
 	},
     hideDetail : function(component, event, helper){
         component.set("v.toRender", false);
     },
     buyItem : function(component, event, helper){
         helper.buyTheItem(component, component.get("v.theItem"));
+    },
+    submitReview : function(component, event, helper){
+        let reviewer = component.get("v.newReview");
+        console.log(component.get("v.newReview"));
+        helper.submitTheReview(component, component.get("v.newReview"));
     }
 })
--- force-app/main/default/aura/productDetail/productDetailHelper.js
+++ force-app/main/default/aura/productDetail/productDetailHelper.js
@@ -1,19 +1,35 @@
 ({
-	getReviews : function(component, theProduct) {
-		let action = component.get("c.getReviewList");
+    getReviews : function(component, theProduct) {
+        let action = component.get("c.getReviewList");
         action.setParams({"sp" : theProduct});
         action.setCallback(this, function(response){
-           let state = response.getState();
+            let state = response.getState();
             if(state === "SUCCESS"){
-                component.set("v.reviewList", response.getReturnValue());
+                let myList = response.getReturnValue();
+                component.set("v.newReview", myList[myList.length -1]);
+                myList.pop();
+                component.set("v.reviewList", myList);
             }else{
                 console.log("Couldn\'t get the reviews...");
             }
         });
         $A.enqueueAction(action);
-	},
+    },
+    submitTheReview : function(component, newReview){
+        let action = component.get("c.submitOrUpdateReview")  
+        action.setParams({"re": newReview});
+        action.setCallback(this, function(response){
+            let state = response.getState();
+            if(state === "SUCCESS"){
+                console.log("Success");
+            }else{
+                console.log("Failure");
+            }
+        });
+        $A.enqueueAction(action);
+    },
     buyTheItem : function(component, theProduct){
-        let buyEvent = $A.get("e.c:ProductToBuy");
+        let buyEvent = $A.get("e.c:addToCart");
         buyEvent.setParams({"product": theProduct});
         buyEvent.fire();
     }
