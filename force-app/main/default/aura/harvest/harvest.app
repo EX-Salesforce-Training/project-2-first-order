b3dc47e4c121dfdad5f62e170ad4374c05a77294
abruce3411 <abruce3411@gmail.com>
2021-05-12 10:38:06 -0500

    deletion

--- force-app/main/default/aura/harvest/harvest.app
+++
@@ -1,3 +0,0 @@
-<aura:application extends="force:slds">
-    <c:TrendingItems />
-</aura:application>
--- force-app/main/default/aura/productReview/productReviewController.js
+++
@@ -1,7 +0,0 @@
-({
-    // submit review for create or update.
-	submitReview : function(component, event, helper){
-        let reviewer = component.get("v.newReview");
-        helper.submitTheReview(component, component.get("v.newReview"));
-    }
-})
