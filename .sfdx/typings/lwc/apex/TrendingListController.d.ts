f0e5ad41b5b91d0fe28f139fbc55470a7b0277ac
jnlongnecker <jlnecker1@gmail.com>
2021-05-12 11:44:32 -0500

    Merge branch 'main' into P2-Alex

---
+++ .sfdx/typings/lwc/apex/TrendingListController.d.ts
@@ -0,0 +1,3 @@
+declare module "@salesforce/apex/TrendingListController.GetProducts" {
+  export default function GetProducts(): Promise<any>;
+}
---
+++ PartyPalace/.sfdx/typings/lwc/product1.resource.d.ts
@@ -0,0 +1,4 @@
+declare module "@salesforce/resourceUrl/product1" {
+    var product1: string;
+    export default product1;
+}
