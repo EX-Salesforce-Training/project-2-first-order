54237b3487d555fea4f0c26841437f4c0a345729
jnlongnecker <jlnecker1@gmail.com>
2021-05-13 13:09:36 -0500

    Master Component and Rentals
    Improved product list
    Improved product
    Created master component
    Created rental products

--- force-app/main/default/aura/PageMaster/PageMasterController.js
+++ force-app/main/default/aura/PageMaster/PageMasterController.js
@@ -1,7 +1,13 @@
 ({
+    doInit : function(component, event, helper) {
+        
+    },
+    
 	turnOnList : function(component, event, helper) {
 		let pList = document.querySelector("#productlist");
+        component.set("v.pListOn", true);
         pList.classList.remove("hidden");
         pList.classList.add("shown");
+        component.find("p-list").set("v.numCols", 3);
 	}
 })
