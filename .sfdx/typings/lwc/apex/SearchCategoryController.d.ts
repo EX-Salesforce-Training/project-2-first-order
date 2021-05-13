fa52072171204378da221a794489cc52441ac8c0
Phong Nguyen <43973882+Phasor2@users.noreply.github.com>
2021-05-13 09:27:51 -0700

    test

---
+++ .sfdx/typings/lwc/apex/SearchCategoryController.d.ts
@@ -0,0 +1,3 @@
+declare module "@salesforce/apex/SearchCategoryController.getPro" {
+  export default function getPro(param: {pageSize: any, pageNumber: any, price: any, rating: any, searchKeyword: any, cbFilter: any}): Promise<any>;
+}
